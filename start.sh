de9983f9cfb060f1771ff3f07881b66abe05dc07
Joshua Buchacher <jbuchacher@me.com>
2017-05-08 13:29:01 -0700

    Initialize camera

--- start.sh
+++ start.sh
@@ -8,4 +8,7 @@ node resin-wifi-connect/src/app.js --clear=false
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
 
+# Make Pi available
+modprobe v4l2_common
+
 python run --config /etc/astrobox/config.yaml
