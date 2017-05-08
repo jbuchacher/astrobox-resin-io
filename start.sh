71b9330e4c224a821f49f5280274e0d32444c147
Joshua Buchacher <jbuchacher@me.com>
2017-05-08 11:05:09 -0700

    Start resin-wifi-connect on boot

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start resin-wifi-connect
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+sleep 1
+node resin-wifi-connect/src/app.js --clear=false
+
+# At this point the WiFi connection has been configured and the device has
+# internet - unless the configured WiFi connection is no longer available.
+
+python run --config /etc/astrobox/config.yml
