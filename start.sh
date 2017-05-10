79e6268df961e56874c77c5f0754f9cd3c8ae0d0
Joshua Buchacher <jbuchacher@me.com>
2017-05-09 20:59:52 -0700

    Sort some stuff out

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 sleep 1
-node /resin-wifi-connect/src/app.js --clear=false
+node /AstroBox/resin-wifi-connect/src/app.js --clear=false
 
 # At this point the WiFi connection has been configured and the device has
 # internet - unless the configured WiFi connection is no longer available.
