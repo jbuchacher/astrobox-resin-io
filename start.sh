6477baa4621e4049ac64361ac7f60dad3c68701e
Joshua Buchacher <jbuchacher@me.com>
2017-05-21 00:14:59 -0700

    Add nginx

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# # Start resin-wifi-connect
-# export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-# sleep 1
-# node /AstroBox/resin-wifi-connect/src/app.js --clear=false
+# Start resin-wifi-connect
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+sleep 1
+node /AstroBox/resin-wifi-connect/src/app.js --clear=false
 
-# # At this point the WiFi connection has been configured and the device has
-# # internet - unless the configured WiFi connection is no longer available.
+# At this point the WiFi connection has been configured and the device has
+# internet - unless the configured WiFi connection is no longer available.
 
 # Make Pi cam available
 modprobe v4l2_common
 modprobe bcm2835-v4l2
 
-cd /opt/janus/share/janus/demos && python -m SimpleHTTPServer 8000 &
-janus --stun-server=stun.l.google.com:19302
+python run --config /etc/astrobox/config.yaml --debug &&
+cd /opt/janus/share/janus/demos && python -m SimpleHTTPServer 8000
