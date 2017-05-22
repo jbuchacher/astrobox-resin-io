98ef9b9f6b6e1697ec2e797f52fb3ac53305e9a1
Joshua Buchacher <jbuchacher@me.com>
2017-05-21 22:38:01 -0700

    fix path?

--- src/astroprint/webrtc/__init__.py
+++ src/astroprint/webrtc/__init__.py
@@ -238,7 +238,7 @@ class WebRtc(object):
 
 					try:
 						response = session.post(
-							url= 'http://0.0.0.0:8088/janus',
+							url= 'http://0.0.0.0:8088',
 							data= {
 							 	"message":{
 									"request": 'info',
